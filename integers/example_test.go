fc84ddbac8242152735f1cd16acbbf836e44ee3b
vladncode <vladncode@gmail.com>
2024-07-21 14:29:48 +0300

    testable examples

---
+++ integers/example_test.go
@@ -0,0 +1,17 @@
+package integers
+
+import (
+	"fmt"
+)
+
+func ExampleAdd() {
+	fmt.Println(Add(2, 2))
+	// Output: 4
+}
+
+func ExampleAdd_second() {
+	fmt.Println(Add(3, 3))
+	// Output: 6
+}
+
+// Testable Examples in Go (godoc) -> https://go.dev/blog/examples
