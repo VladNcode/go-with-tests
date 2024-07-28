128690b2b42dc126098e5171b20aa07ca8f776eb
vladncode <vladncode@gmail.com>
2024-07-28 11:00:28 +0300

    structs

---
+++ structs-methods-interfaces/geometry_test.go
@@ -0,0 +1,54 @@
+package main
+
+import "testing"
+
+type Shape interface {
+	Area() float64
+}
+
+func TestArea(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+
+}
+
+func TestAreaTableTest(t *testing.T) {
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		// {Rectangle{12, 6}, 72.0},
+		// {Circle{10}, 314.1592653589793},
+		// {Triangle{12, 6}, 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.name, got, tt.want)
+			}
+		})
+	}
+
+}
